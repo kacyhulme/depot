b9c1db61f4f58d1f856f6e86aac16ec98db08004
Kacy Hulme <kacyhulme@gmail.com>
2012-10-09 15:11:09 -0500

    'paginate_p176_take2'

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -2,7 +2,8 @@ class LineItem < ActiveRecord::Base
 	belongs_to :order
 	belongs_to :product
 	belongs_to :cart
-
+	attr_accesssible :cart_id, :product_id
+	
 	def total_price
 		product.price * quantity
 	end
